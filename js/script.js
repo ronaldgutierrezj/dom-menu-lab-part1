906989883fb3e2b12c7eab18c733d3c877d523a6
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2022-04-20 10:08:04 -0500

    1st part finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
 const mainEl = document.querySelector("main")
 mainEl.style.backgroundColor = 'var(--main-bg)'
 mainEl.innerHTML = "<h1>SEI ROCKS!</h1>"
+mainEl.classList.add("flex-ctr")
+
+const topMenuEL = document.querySelector("nav#top-menu")
+
+topMenuEL.style.height = "100%"
+topMenuEL.style.backgroundColor = "var(--top-menu-bg)"
+topMenuEL.classList.add("flex-around")
+
+for (menu of menuLinks){
+    const a = document.createElement("a")
+    a.setAttribute("href", menu.href)
+    a.innerHTML = menu.text
+    topMenuEL.append(a)
+}
